6a1799ad347ba9adce849a745f63aee6ce9835c4
Ryan Holdstock <pasberryrie@gmail.com>
2026-02-27 14:29:40 -0500

    added vm mounting on launch

--- Scripts/launchitsc3146.sh
+++ Scripts/launchitsc3146.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin/limactl
 kitty -e limactl shell itsc-3146
+sshfs -F ~/.lima/itsc-3146/ssh.config lima-itsc-3146:/home/itsc ~/mnt/vm
