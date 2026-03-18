017afb14e3c55d37ab5c0f0ee2fa514c54f07486
Ryan Holdstock <pasberryrie@gmail.com>
2026-03-18 14:19:22 -0400

    031826 changes

--- Scripts/launchitsc3146.sh
+++ Scripts/launchitsc3146.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 
 limactl start itsc-3146
-
+limactl shell itsc-3146 -- true
 sshfs -v -F ~/.lima/itsc-3146/ssh.config lima-itsc-3146:/home/itsc ~/mnt/schoolvm/
 echo "sshfs exit code: $?"
 
