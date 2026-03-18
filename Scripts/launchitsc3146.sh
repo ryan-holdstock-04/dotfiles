240e145dd3d67cbb4c7a7edf69b1e6a62780a065
Ryan Holdstock <pasberryrie@gmail.com>
2026-03-18 17:24:59 -0400

    hypridle tweaks

--- Scripts/launchitsc3146.sh
+++ Scripts/launchitsc3146.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
-
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin:/usr/bin:/usr/local/bin
 limactl start itsc-3146
-limactl shell itsc-3146 -- true
-sshfs -v -F ~/.lima/itsc-3146/ssh.config lima-itsc-3146:/home/itsc ~/mnt/schoolvm/
-echo "sshfs exit code: $?"
-
 kitty -e limactl shell itsc-3146
